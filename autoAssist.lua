f311f39b8ea2f49bdcfc5c847aa21c7437684815
Michael Davidson <enfarious@gmail.com>
2022-06-20 13:31:01 -0400

    Adjustments to order of ops Made auto-fastfollow more reliable

--- autoAssist.lua
+++ autoAssist.lua
@@ -40,7 +40,7 @@ defaults.show_debug = false
 defaults.approach = true
 defaults.max_range = 3.5
 defaults.face_target = true
-defaults.update_time = 0.03
+defaults.update_time = 0.25
 defaults.assist_target = nil
 defaults.engage = true
 defaults.engage_delay = 1
@@ -52,7 +52,6 @@ local running = false
 local approaching = false
 local player = windower.ffxi.get_player()
 local player_body = windower.ffxi.get_mob_by_id(player.id)
-local assist_target = nil
 local mob = nil
 local start_position = {x=nil, y=nil}
 local is_following = false
@@ -70,7 +69,7 @@ end
 function message(str, debug_msg)
     if (debug_msg) then
         if (settings.show_debug and settings.show_debug == true) then
-            windower.add_to_chat(207, _addon.name.." (debug_msg): "..str)
+            windower.add_to_chat(207, _addon.name.." (debug): "..str)
         end
         return
     end
@@ -110,8 +109,8 @@ function is_disabled()
 end
 
 function engage()
+    local assist_target = nil
     if (settings.assist_target and settings.assist_target ~= '') then
-        message(settings.assist_target, true)
         assist_target = windower.ffxi.get_mob_by_name(settings.assist_target)
     else
         return
@@ -121,12 +120,7 @@ function engage()
         if (not mob) then -- or not mob.claim_id or mob.claim_id == 0) then
             return
         end
-        -- Stop following our assist target before we engage
-        if (settings.use_fastfollow == true and settings.follow_target and settings.follow_target ~= "") then
-            is_following = false
-            windower.send_command("input //ffo stop") -- Stop fastfollow if it's running
-        end
-    
+
         -- Assist our assist target and then engage
         local tgt = windower.ffxi.get_mob_by_target('t')
         if (not tgt or tgt.id ~= mob.id) then
@@ -156,9 +150,8 @@ function is_facing_target()
     local player_body = windower.ffxi.get_mob_by_id(player.id)
     local angle = (math.atan2((mob.y - player_body.y), (mob.x - player_body.x))*180/math.pi)
     local heading = player_body.heading*180/math.pi*-1
-    message("To face target: "..tostring(angle).." degrees".." current: "..heading, true)
 
-    if (math.abs(math.abs(heading) - math.abs(angle)) < 15) then
+    if (math.abs(math.abs(heading) - math.abs(angle)) < 5) then
         return true
     end
     return false
@@ -187,10 +180,8 @@ function is_in_range()
     end
     local dist = mob.distance:sqrt() - (mob.model_size/2 + windower.ffxi.get_mob_by_id(player.id).model_size/2 - 1)
     if (dist > settings.max_range) then
-        message("Out of Range: Max="..settings.max_range.." < Current="..dist.."", true)
         return false
     end
-    message("In Range", true)
     return true
 end
 
@@ -210,7 +201,6 @@ function approach(start)
         approaching = true
         return
     else
-        message("Aproach done.", true)
         windower.ffxi.run(false)
         approaching = false
     end
@@ -227,23 +217,19 @@ end
 function in_position()
     player = windower.ffxi.get_player()
     local player_body = windower.ffxi.get_mob_by_id(player.id)
-    if (not player_body) then
+    if (not player_body or use_fastfollow == true) then
         return true
     end
     local dist = ((player_body.x - start_position.x)^2 + (player_body.y - start_position.y)^2):sqrt()
     if (dist <= 2) then
-        message("At start position "..dist.."' away.", true)
         reposition(false)
         return true
     end
-    message(dist.."' from start position", true)
-
     return false
 end
 
 function reposition(start)
     if (start) then
-        message("Returning to position.", true)
         local player_body = windower.ffxi.get_mob_by_id(player.id)
         local angle = (math.atan2((start_position.y - player_body.y), (start_position.x - player_body.x))*180/math.pi)*-1
         local rads = angle:radian()
@@ -282,7 +268,15 @@ windower.register_event('prerender', function(...)
     next_check_time = time + settings.update_time
 
     player = windower.ffxi.get_player()
+    assist = windower.ffxi.get_mob_by_name(settings.assist_target)
+
     if (mob and player.status == 1) then 
+        if (is_following and settings.use_fastfollow == true and settings.follow_target and settings.follow_target ~= "") then
+            message("Disabling Fastfollow!", true)
+            windower.send_command("ffo stop")
+            --windower.chat.input("//ffo stop")
+            is_following = false
+        end
         if (not is_facing_target() and settings.face_target == true) then
             face_target()
         end
@@ -290,13 +284,15 @@ windower.register_event('prerender', function(...)
             approach(true)
         end
         return
-    elseif (player.status ~= 1 and not is_following and settings.use_fastfollow == true and settings.follow_target and settings.follow_target ~= "") then
-        windower.send_command("input //ffo "..settings.follow_target)
-        is_following = true
-    elseif (not in_position() and settings.reposition == true) then
-        reposition(true)
-    elseif (player.status == 0 and not is_disabled()) then
+    elseif (assist.status == 1 and player.status == 0 and not is_disabled()) then
         engage()
+    elseif (player.status ~= 1 and not is_following and settings.use_fastfollow == true and settings.follow_target and settings.follow_target ~= "") then
+        message("Enabling Fastfollow!", true)
+        windower.send_command("ffo "..settings.follow_target)
+        --windower.chat.input("//ffo "..settings.follow_target)
+        is_following = true
+    elseif (player.status == 0 and not is_disabled() and not is_following and not in_position() and settings.reposition == true) then
+        reposition(true)
     end
 end)
 
